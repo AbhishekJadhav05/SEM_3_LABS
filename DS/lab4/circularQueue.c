7c7cefb242dee7a9c72c3088d1a29dd5bc6c9c03
Abhishek <abhishek.jadhav.general@gmail.com>
2024-08-28 11:15:08 +0530

    completed circular queue

--- DS/lab4/circularQueue.c
+++ DS/lab4/circularQueue.c
@@ -35,41 +35,13 @@ void display(){
         printf("queue underflow");
         return; 
     }
-    for(int i=circularQueue.f+1;i<=length;i--){
-            printf("%d",circularQueue.arr[i]);
-        }
-    
+    for(int i=0;i<length;i++){
+            printf("%d",circularQueue.arr[(circularQueue.f+i+1)%MAX]);
+        }    
 }
 
 void main(){
     circularQueue.f = -1;
     circularQueue.r = -1;
-    enque(10);
-    enque(20);
-    enque(30);
-    display();
-    printf("\n");  // Should print: 10 20 30
-    
-    deque();    // Removes 10
-    display();  // Should print: 20 30
-    
-    printf("\n");
-    enque(40);
-    enque(50);
-    display();  // Should print: 20 30 40 50
-    
-    printf("\n");
-    enque(60);  // Should wrap around
-    display();  // Should print: 20 30 40 50 60
-    
-    printf("\n");
-    enque(70);  // Should cause overflow
-    deque();    // Removes 20
-    display();  // Should print: 30 40 50 60
-    
-    printf("\n");
-    enque(70);  // Should succeed now
-    display();
-    
-    printf("\n");  // Should print: 30 40 50 60 70
+    //call functions here.
 }
