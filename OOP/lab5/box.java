ae837ee216b1efed6db9af5e61e6bb812e36646e
Abhishek Jadhav <76618289+AbhishekJadhav05@users.noreply.github.com>
2024-08-29 16:28:28 +0530

    Create box.java

---
+++ OOP/lab5/box.java
@@ -0,0 +1,22 @@
+package Lab5;
+
+import java.util.Scanner;
+
+public class box {
+    double width,height,depth;
+    box(double width,double height,double depth){
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+    double volume(){
+        return this.height*this.width*this.depth;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        box box = new box(scanner.nextDouble(),scanner.nextDouble(), scanner.nextDouble());
+        System.out.println(box.volume());
+        scanner.close();
+    }
+}
