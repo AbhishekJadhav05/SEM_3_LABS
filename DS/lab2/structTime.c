d5ec33c4e808ec8868dd20c41aebb867d097659e
Abhishek <abhishek.jadhav.general@gmail.com>
2024-09-10 06:05:37 +0530

    fixed struct time lab2 question

--- DS/lab2/structTime.c
+++ DS/lab2/structTime.c
@@ -1,33 +1,76 @@
 #include<stdio.h>
-#include<stdlib.h>
 struct time{
     int second;
     int minute;
     int hour;
-}s[2];
+}time[2];
+
 void read(){
     for(int i=0;i<=1;i++){
-        printf("enter seconds : ");
-        fflush(stdin);
-        scanf("%d",time[i].second); // is scanf("%d",time[n]) and scanf("%d",time[n][0]) same base address ?
-        printf("enter second: ");
-        scanf("%d",time[i].minute);
-        fflush(stdin);
         printf("enter hour : ");
-        scanf("%d",time[i].hour);
+        scanf("%d",&time[i].hour);
+        printf("enter minutes: ");
+        scanf("%d",&time[i].minute);
+        printf("enter seconds : ");
+        scanf("%d",&time[i].second); // is scanf("%d",time[n]) and scanf("%d",time[n][0]) same base address ?
     }
 }
-void display(struct time* time,int n){
-
-        for(int i=0;i<n;i++){
-        printf("second : %d \n",time[i].second);
-        printf("minute: %d \n",time[i].minute);
-        printf("hour : %d \n",time[i].hour);
-        printf("--------------------------------- \n");
-        }
+void display(struct time time){
+        printf("hour : %d \n",time.hour);
+        printf("minute: %d \n",time.minute);
+        printf("second : %d \n",time.second);
+        printf("\n");
 }
+        
+struct time add(struct time* time1, struct time* time2) {
+    struct time result;
+    
+    result.second = time1->second + time2->second;
+    result.minute = time1->minute + time2->minute;
+    result.hour = time1->hour + time2->hour;
+    
+    // Handle seconds overflow
+    if (result.second >= 60) {
+        result.second -= 60;
+        result.minute++;
+    }
 
+    // Handle minutes overflow
+    if (result.minute >= 60) {
+        result.minute -= 60;
+        result.hour++;
+    }
 
+    return result;
+}
+struct time diff(struct time* time1, struct time* time2) {
+    struct time result;
+    
+    // Initialize result
+    result.hour = time1->hour - time2->hour;
+    result.minute = time1->minute - time2->minute;
+    result.second = time1->second - time2->second;
+
+    // Handle seconds underflow
+    if (result.second < 0) {
+        result.second += 60;
+        result.minute--;
+    }
+
+    // Handle minutes underflow
+    if (result.minute < 0) {
+        result.minute += 60;
+        result.hour--;
+    }
+
+    return result;
+}
 void main(){
-    read(time,n);
-    display(time,n);
+    printf("enter both time objects :\n");
+    read();
+    printf("addition of time :\n");
+    display(add(&time[0],&time[1]));
+    printf("\n");
+    printf("subtraction of time :\n");
+    display(diff(&time[0],&time[1]));
+}
